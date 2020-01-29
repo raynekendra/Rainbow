dd1c033983cfe47dad81a134109b2306e868932d
kendra rayne <raynekendra@gmail.com>
2020-01-28 23:03:28 -0500

    Set up table view with colors array

---
+++ Rainbow/Model/Color.swift
@@ -0,0 +1,15 @@
+//
+//  Color.swift
+//  Rainbow
+//
+//  Created by Kendra McVay on 1/28/20.
+//  Copyright © 2020 Kendra McVay. All rights reserved.
+//
+
+
+import UIKit
+
+struct MyColor {
+    let name: String
+    let color: UIColor
+}
---
+++ Rainbow/View Controllers/ColorsTableViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ColorsTableViewController.swift
+//  Rainbow
+//
+//  Created by Kendra McVay on 1/28/20.
+//  Copyright © 2020 Kendra McVay. All rights reserved.
+//
+
+import UIKit
+
+class ColorsTableViewController: UITableViewController {
+
+    var colors: [MyColor] = [
+        MyColor(name: "Red", color: .red),
+        MyColor(name: "Orange", color: .orange),
+        MyColor(name: "Yellow", color: .yellow),
+        MyColor(name: "Green", color: .green),
+        MyColor(name: "Blue", color: .blue),
+        MyColor(name: "Indigo", color: .cyan),
+        MyColor(name: "Violet", color: .purple)
+    ]
+
+    // MARK: - Table view data source
+
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return colors.count
+    }
+// Index Path (x = section, y = row)
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ColorCell", for: indexPath)
+        
+        let color = colors[indexPath.row]
+        cell.textLabel?.text = color.name
+        cell.backgroundColor = color.color
+        return cell
+    }
+   
+   
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+ 
+
+}

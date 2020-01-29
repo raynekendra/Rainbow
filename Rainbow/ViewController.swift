d53f9a29734950d9826da183fe229c01f778038a
kendra rayne <raynekendra@gmail.com>
2020-01-28 21:43:15 -0500

    Initial Commit

---
+++ Rainbow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rainbow
+//
+//  Created by Kendra McVay on 1/28/20.
+//  Copyright © 2020 Kendra McVay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
